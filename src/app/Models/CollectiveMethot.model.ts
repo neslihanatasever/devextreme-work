80b4ec5c4574a83fa3957bd6a94de65932478556
neslihan <neslihan_atasvr111@outlook.com>
2022-06-20 14:02:23 +0300

    Delete src/app/Models directory

--- src/app/Models/CollectiveMethot.model.ts
+++
@@ -1,9 +0,0 @@
-export class CollectiveMethod {
-  method: any; // 1 get // 2 post //3 Put // 4 Delete
-  path: any;
-  jsonData: any = {};
-  code = false;
-  // @ts-ignore
-  successMessage: string = null;
-  errorMessage = '-l?(';
-}
